bb438458c5beb6d4a61845666ee1a7d71fb73925
internetcharles <charlesodellsmith@gmail.com>
2021-05-11 14:10:03 -0700

    solution 2 and comments

--- src/solutions/solution2.js
+++ src/solutions/solution2.js
@@ -1,6 +1,6 @@
 var flavors = "LOADING!";
 
-// Div is added on DOM load.
+// Initial loading div creation
 document.addEventListener('DOMContentLoaded', function() {
   var div = document.createElement('div');
   div.id = 'flavors';
@@ -9,13 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
   document.body.appendChild(div);
 }, false);
 
-// Temp variable is declared for assignment of child divs
 let temp;
 
 executeFlavorChange = () => {
   flavorDiv = document.getElementById('flavors');
   flavorDiv.innerHTML = "Your flavors are:";
-  // For loop iterates through flavors array and creates div for each
   for (i = 0; i < flavors.length; i++) {
     temp = document.createElement('div');
     temp.className = 'flavor';
@@ -24,16 +22,10 @@ executeFlavorChange = () => {
   }
 }
 
-const interval = setInterval(() => {
-  // Set interval checks if flavors array is empty
-  if (window.iceCreamFlavors.length > 0) {
+// Timeout occurs after maximum loading time. Flavors are changed to the window variable.
+setTimeout(() => {
     flavors = window.iceCreamFlavors;
     console.log("FLAVORS HAVE CHANGED" + flavors);
     executeFlavorChange();
-    clearInterval(interval);
-  }
-  else {
-    console.log('Flavors havent changed yet');
-  }
-}, 500)
+}, 1450)
 
